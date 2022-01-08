c45244f12763b624765995f6da2cbf64ac3e73b9
Xiao Ma <mahimself@gmail.com>
2022-01-08 17:21:24 -0500

    * use Compose for group activity * a bit ui polishing for group list

--- app/src/main/java/com/xiaoism/time/ui/main/group/GroupActivity.kt
+++ app/src/main/java/com/xiaoism/time/ui/main/group/GroupActivity.kt
@@ -1,38 +1,79 @@
 package com.xiaoism.time.ui.main.group
 
-import android.app.ActivityGroup
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.platform.ComposeView
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.xiaoism.time.R
 import com.xiaoism.time.databinding.ActivityGroupBinding
 import com.xiaoism.time.model.GroupWithPersons
+import com.xiaoism.time.model.PersonWithCity
 
-class GroupActivity : AppCompatActivity() {
+class GroupActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding =
-            DataBindingUtil.setContentView<ActivityGroupBinding>(this, R.layout.activity_group)
-        binding.lifecycleOwner = this
-
-        val recyclerView = binding.recyclerview
-        val adapter = GroupAdapter(this)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val group: GroupWithPersons? = intent.getParcelableExtra("group") as? GroupWithPersons
         Log.d("grou view", group.toString())
 
         group?.let {
-            binding.data = it
-            adapter.setGroup(it)
+            setContent {
+                content(group = it)
+            }
+        }
+
+    }
+
+    @Composable
+    private fun content(group: GroupWithPersons) {
+        Column() {
+            Text(
+                "${group.group.name}",
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp)
+            )
+            LazyColumn {
+                items(group.persons) { person ->
+                    row(person)
+                    Divider(color = Color.Gray, thickness = 6.dp)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun row(person: PersonWithCity) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 6.dp)
+        ) {
+            Text(person.person.name, fontSize = 16.sp)
+            Text(person.city!!.name, fontSize = 13.sp, color = Color.LightGray)
         }
     }
 }
--- app/src/main/java/com/xiaoism/time/ui/main/group/GroupListFragment.kt
+++ app/src/main/java/com/xiaoism/time/ui/main/group/GroupListFragment.kt
@@ -6,20 +6,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 
@@ -28,7 +34,7 @@ import com.xiaoism.time.model.GroupWithPersons
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GroupListFragment : Fragment(), OnGroupClickListener {
+class GroupListFragment : Fragment() {
     private val viewModel by viewModels<GroupListViewModel>()
 
     override fun onCreateView(
@@ -36,30 +42,6 @@ class GroupListFragment : Fragment(), OnGroupClickListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        val binding = DataBindingUtil.inflate<FragmentGroupsBinding>(
-//            inflater, R.layout.fragment_groups, container, false
-//        )
-//        binding.lifecycleOwner = this
-//
-//        val recyclerView = binding.groupList
-//        val adapter = GroupListAdapter(requireContext(), this)
-//        recyclerView.adapter = adapter
-//        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-//
-//        viewModel = ViewModelProvider(this).get(GroupListViewModel::class.java)
-//        viewModel.groups.observe(viewLifecycleOwner, Observer { groups ->
-//            groups?.let {
-//                adapter.setGroups(groups)
-//            }
-//        })
-//
-//        val fab = binding.addGroup;
-//        fab.setOnClickListener {
-//            createGroup()
-//        }
-//
-//        return binding.root
-
         return ComposeView(requireContext()).apply {
             setContent {
                 Scaffold(floatingActionButton = { createBtn() }, content = { listContent() })
@@ -71,14 +53,12 @@ class GroupListFragment : Fragment(), OnGroupClickListener {
     private fun listContent() {
         val groups by viewModel.groups.observeAsState(initial = emptyList())
 
-
         LazyColumn {
             items(groups) { group ->
                 groupRow(group = group)
+                Divider(color = Color.Gray, thickness = 6.dp)
             }
         }
-
-
     }
 
     @Composable
@@ -93,12 +73,23 @@ class GroupListFragment : Fragment(), OnGroupClickListener {
 
     @Composable
     private fun groupRow(group: GroupWithPersons) {
-        Row {
-            Text(text = "${group.group.name}")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 6.dp)
+                .clickable { onItemClick(group) },
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "${group.group.name}",
+                fontSize = 20.sp
+            )
+            Text(text = "${group.persons.size}")
         }
     }
 
-    override fun onItemClick(group: GroupWithPersons) {
+    private fun onItemClick(group: GroupWithPersons) {
         val intent = Intent(activity, GroupActivity::class.java)
         intent.putExtra("group", group)
         startActivity(intent)
