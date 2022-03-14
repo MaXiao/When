f8c8989e1a78b46879cb93320485a6f99871f830
Xiao Ma <mahimself@gmail.com>
2022-03-13 22:05:42 -0400

    change add city from xml to compose

--- app/src/main/java/com/xiaoism/time/ui/city/AddCityActivity.kt
+++ app/src/main/java/com/xiaoism/time/ui/city/AddCityActivity.kt
@@ -3,57 +3,79 @@ package com.xiaoism.time.ui.city
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.textfield.TextInputEditText
-import com.xiaoism.time.R
-import com.xiaoism.time.databinding.FragmentCityBinding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.xiaoism.time.model.City
 import com.xiaoism.time.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AddCityActivity : AppCompatActivity(), OnCityClickListener {
+class AddCityActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding =
-            DataBindingUtil.setContentView<FragmentCityBinding>(this, R.layout.fragment_city)
-        binding.lifecycleOwner = this
-
-        val recyclerView = binding.recyclerview
-        val adapter = CityAdapter(this, this)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        viewModel.cities.observe(this, Observer { cities ->
-            cities?.let { adapter.setCities(cities) }
-        })
-
-        val input = findViewById<TextInputEditText>(R.id.input)
-        input.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                s.let {
-                    viewModel.searchCity(it.toString())
-                }
-            }
-        })
+        setContent {
+            Scaffold(content = { Content(viewModel) })
+        }
     }
 
-    override fun onItemClick(city: City) {
+    @Composable
+    fun Content(viewModel: MainViewModel) {
+        var value by remember { mutableStateOf("") }
+        val cities by viewModel.cities.observeAsState(initial = emptyList())
+
+        Column() {
+            TextField(
+                value = value,
+                onValueChange = {
+                    value = it
+                    viewModel.searchCity(it)
+                },
+                label = { Text("Enter city") },
+                maxLines = 1,
+                textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(vertical = 20.dp).fillMaxWidth()
+            )
+
+            LazyColumn() {
+                items(cities) {
+                    Text(
+                        text = "${it.name}, ${it.country}",
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(vertical = 10.dp, horizontal = 12.dp)
+                            .weight(1f)
+                            .fillMaxWidth()
+                            .clickable { onItemClick(it) }
+                    )
+                }
+            }
+        }
+    }
+
+    private fun onItemClick(city: City) {
         val intent = Intent()
         intent.putExtra(CityActivityContract.CITY, city)
         setResult(Activity.RESULT_OK, intent)
